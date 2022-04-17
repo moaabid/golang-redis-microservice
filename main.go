61adce30572ec0efef61d6dedbd900e72bd4b20e
moaabid <moaabid153@gmail.com>
2022-04-17 20:00:21 +0530

    added redis

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/url"
+	"os"
+	"time"
+
+	"github.com/go-redis/redis/v8"
 )
 
 type APIResponse struct {
@@ -29,23 +35,53 @@ type NominatimResponse struct {
 func main() {
 	fmt.Println("Starting Server")
 
+	api := NewAPI()
 	//localhost:8080/api?q=san%20francisco
-	http.HandleFunc("/api", Handler)
+	http.HandleFunc("/api", api.Handler)
 
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(os.Getenv("PORT"), nil)
 }
 
-func Handler(w http.ResponseWriter, r *http.Request) {
+type API struct {
+	cache *redis.Client
+}
 
+func NewAPI() *API {
+	var opts *redis.Options
+
+	if os.Getenv("LOCAL") == "true" {
+		redisAddress := fmt.Sprintf("%s:6379", os.Getenv("REDIS_URL"))
+		opts = &redis.Options{
+			Addr:     redisAddress,
+			Password: "", // no password set
+			DB:       0,  // use default DB
+		}
+	} else {
+		buildOpts, err := redis.ParseURL(os.Getenv("REDIS_URL"))
+		if err != nil {
+			panic(err)
+		}
+
+		opts = buildOpts
+	}
+	rdb := redis.NewClient(opts)
+
+	return &API{
+		cache: rdb,
+	}
+}
+
+func (a *API) Handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Hitted endpoint")
 	city := r.URL.Query().Get("city")
-	data, err := getData(city)
+	data, isCached, err := a.getData(r.Context(), city)
 
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 	resp := APIResponse{
-		Cache: false,
+		Cache: isCached,
 		Data:  data,
 	}
 	err = json.NewEncoder(w).Encode(resp)
@@ -56,24 +92,48 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func getData(city string) ([]NominatimResponse, error) {
-	escape := url.PathEscape(city)
+func (a *API) getData(ctx context.Context, city string) ([]NominatimResponse, bool, error) {
 
-	address := fmt.Sprintf("https://nominatim.openstreetmap.org/search?q=%s&format=json", escape)
+	value, err := a.cache.Get(ctx, city).Result()
+	if err == redis.Nil {
+		escape := url.PathEscape(city)
 
-	resp, err := http.Get(address)
+		address := fmt.Sprintf("https://nominatim.openstreetmap.org/search?q=%s&format=json", escape)
 
-	if err != nil {
-		return nil, err
+		resp, err := http.Get(address)
+
+		if err != nil {
+			return nil, false, err
+		}
+
+		data := make([]NominatimResponse, 0)
+
+		err = json.NewDecoder(resp.Body).Decode(&data)
+
+		if err != nil {
+			return nil, false, err
+		}
+		dataBytes, err := json.Marshal(data)
+		if err != nil {
+			return nil, false, err
+		}
+		//Set value in Redis
+		err = a.cache.Set(ctx, city, bytes.NewBuffer(dataBytes).Bytes(), time.Second*15).Err()
+		if err != nil {
+			return nil, false, err
+		}
+		//Return value
+		return data, false, err
+	} else if err != nil {
+		fmt.Printf("Error on calling redis: %v\n", err)
+		return nil, false, err
+	} else {
+		data := make([]NominatimResponse, 0)
+		err := json.Unmarshal(bytes.NewBufferString(value).Bytes(), &data)
+		if err != nil {
+			return nil, false, err
+		}
+		return data, true, err
 	}
 
-	data := make([]NominatimResponse, 0)
-
-	err = json.NewDecoder(resp.Body).Decode(&data)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return data, err
 }
